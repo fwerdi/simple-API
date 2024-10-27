c0280f35a221dd319d5f56684a2cf1a682d547f5
fwerdi <149984400+fwerdi@users.noreply.github.com>
2024-10-27 19:17:14 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const Product = require("./models/product.model.js");
+const productRoute = require("./routes/product.route.js");
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+
+//routes
+app.use("/api/products", productRoute);
+
+app.get("/", (req, res) => {
+  res.send("Hello from Node API Server Updated");
+});
+
+
+
+mongoose
+  .connect(
+    "mongodb+srv://ferdiansya3007:paxi4LXr1duxSdZO@backenddb.ryozl.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB"
+  )
+  .then(() => {
+    console.log("Connect to Database!");
+    app.listen(3000, () => {
+      console.log("Server is running on port 3000");
+    });
+  })
+  .catch(() => {
+    console.log("Connect Failed");
+  });
